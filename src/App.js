ce9fed4e2d757df9f42a8ebf7dad49cb93870c3a
SerAyaKz <ayanbek.as@gmail.com>
2024-08-22 17:13:46 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
+import {Hero} from './images'; 
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="hero-container">
+        <img src={Hero} className="hero-gif" alt="Hero" />
+        <input type="text" className="overlay-input" placeholder="?" />
+      </div>
     </div>
+    
   );
 }
 
---
+++ src/constants/index.jsx
@@ -0,0 +1,18 @@
+export const REDIRECT_OPTIONS = [
+    {
+        "prompt": "SALUT",
+        "link": "https://www.linkedin.com/in/ayanbek-serikov-994502185/"
+      },
+      {
+        "prompt": "EPIC",
+        "link": "https://www.youtube.com/watch?v=qq09UkPRdFY"
+      },
+      {
+        "prompt": "LORE",
+        "link": "https://en.wikipedia.org/wiki/Nuckelavee"
+      },
+      {
+        "prompt": "ORACLE",
+        "link": "https://www.google.com/search?q=Prophecies+of+the+Tiburtina+Sibyl"
+      }
+];
